9ff02b9012f1fa5315a0797474e6d2ffe8a530a6
Lucas Biagini <biaginisilva@gmail.com>
2018-11-17 11:29:06 -0300

    Added code to build symboltable

--- atividade5/BuildSymbolTableVisitor.java
+++ atividade5/BuildSymbolTableVisitor.java
@@ -1,39 +1,6 @@
 package br.ufpe.cin.if688.minijava.atividade5;
 
-import br.ufpe.cin.if688.minijava.ast.And;
-import br.ufpe.cin.if688.minijava.ast.ArrayAssign;
-import br.ufpe.cin.if688.minijava.ast.ArrayLength;
-import br.ufpe.cin.if688.minijava.ast.ArrayLookup;
-import br.ufpe.cin.if688.minijava.ast.Assign;
-import br.ufpe.cin.if688.minijava.ast.Block;
-import br.ufpe.cin.if688.minijava.ast.BooleanType;
-import br.ufpe.cin.if688.minijava.ast.Call;
-import br.ufpe.cin.if688.minijava.ast.ClassDeclExtends;
-import br.ufpe.cin.if688.minijava.ast.ClassDeclSimple;
-import br.ufpe.cin.if688.minijava.ast.False;
-import br.ufpe.cin.if688.minijava.ast.Formal;
-import br.ufpe.cin.if688.minijava.ast.Identifier;
-import br.ufpe.cin.if688.minijava.ast.IdentifierExp;
-import br.ufpe.cin.if688.minijava.ast.IdentifierType;
-import br.ufpe.cin.if688.minijava.ast.If;
-import br.ufpe.cin.if688.minijava.ast.IntArrayType;
-import br.ufpe.cin.if688.minijava.ast.IntegerLiteral;
-import br.ufpe.cin.if688.minijava.ast.IntegerType;
-import br.ufpe.cin.if688.minijava.ast.LessThan;
-import br.ufpe.cin.if688.minijava.ast.MainClass;
-import br.ufpe.cin.if688.minijava.ast.MethodDecl;
-import br.ufpe.cin.if688.minijava.ast.Minus;
-import br.ufpe.cin.if688.minijava.ast.NewArray;
-import br.ufpe.cin.if688.minijava.ast.NewObject;
-import br.ufpe.cin.if688.minijava.ast.Not;
-import br.ufpe.cin.if688.minijava.ast.Plus;
-import br.ufpe.cin.if688.minijava.ast.Print;
-import br.ufpe.cin.if688.minijava.ast.Program;
-import br.ufpe.cin.if688.minijava.ast.This;
-import br.ufpe.cin.if688.minijava.ast.Times;
-import br.ufpe.cin.if688.minijava.ast.True;
-import br.ufpe.cin.if688.minijava.ast.VarDecl;
-import br.ufpe.cin.if688.minijava.ast.While;
+import br.ufpe.cin.if688.minijava.ast.*;
 import br.ufpe.cin.if688.minijava.symboltable.Class;
 import br.ufpe.cin.if688.minijava.symboltable.Method;
 import br.ufpe.cin.if688.minijava.symboltable.SymbolTable;
@@ -67,9 +34,17 @@ public class BuildSymbolTableVisitor implements IVisitor<Void> {
 	// Identifier i1,i2;
 	// Statement s;
 	public Void visit(MainClass n) {
-		n.i1.accept(this);
-		n.i2.accept(this);
+
+		this.symbolTable.addClass(n.i1.s, null);
+		this.currClass = this.symbolTable.getClass(n.i1.s);
+		this.currClass.addMethod("main", null);
+		this.currMethod = this.currClass.getMethod("main");
+		this.currMethod.addParam(n.i2.s, null);
 		n.s.accept(this);
+
+//		n.i1.accept(this);
+//		n.i2.accept(this);
+//		n.s.accept(this);
 		return null;
 	}
 
@@ -77,13 +52,32 @@ public class BuildSymbolTableVisitor implements IVisitor<Void> {
 	// VarDeclList vl;
 	// MethodDeclList ml;
 	public Void visit(ClassDeclSimple n) {
-		n.i.accept(this);
-		for (int i = 0; i < n.vl.size(); i++) {
-			n.vl.elementAt(i).accept(this);
+
+		if (!this.symbolTable.containsClass(n.i.s)) {
+
+			this.symbolTable.addClass(n.i.s, null);
+			this.currClass = symbolTable.getClass(n.i.s);
+			this.currMethod = null;
+			for (int i = 0; i < n.vl.size(); i++) {
+				n.vl.elementAt(i).accept(this);
+			}
+			for (int i = 0; i < n.ml.size(); i++) {
+				n.ml.elementAt(i).accept(this);
+			}
+
+		} else {
+			System.out.println("Classe " + n.i.s + " ja definida no escopo atual");
+			System.exit(0);
 		}
-		for (int i = 0; i < n.ml.size(); i++) {
-			n.ml.elementAt(i).accept(this);
-		}
+
+
+//		n.i.accept(this);
+//		for (int i = 0; i < n.vl.size(); i++) {
+//			n.vl.elementAt(i).accept(this);
+//		}
+//		for (int i = 0; i < n.ml.size(); i++) {
+//			n.ml.elementAt(i).accept(this);
+//		}
 		return null;
 	}
 
@@ -92,22 +86,66 @@ public class BuildSymbolTableVisitor implements IVisitor<Void> {
 	// VarDeclList vl;
 	// MethodDeclList ml;
 	public Void visit(ClassDeclExtends n) {
-		n.i.accept(this);
-		n.j.accept(this);
-		for (int i = 0; i < n.vl.size(); i++) {
-			n.vl.elementAt(i).accept(this);
+
+		if (!this.symbolTable.containsClass(n.i.s)) {
+			if (this.symbolTable.containsClass(n.j.s)) {
+
+				this.symbolTable.addClass(n.i.s, n.j.s);
+				this.currClass = this.symbolTable.getClass(n.i.s);
+				this.currMethod = null;
+				for (int i = 0; i < n.vl.size(); i++) {
+					n.vl.elementAt(i).accept(this);
+				}
+				for (int i = 0; i < n.ml.size(); i++) {
+					n.ml.elementAt(i).accept(this);
+				}
+
+			} else {
+				System.out.println("Classe " + n.j.s + " nao definida no escopo atual");
+				System.exit(0);
+			}
+		} else {
+			System.out.println("Classe " + n.i.s + " ja definida no escopo atual");
+			System.exit(0);
 		}
-		for (int i = 0; i < n.ml.size(); i++) {
-			n.ml.elementAt(i).accept(this);
-		}
+
+
+//		n.i.accept(this);
+//		n.j.accept(this);
+//		for (int i = 0; i < n.vl.size(); i++) {
+//			n.vl.elementAt(i).accept(this);
+//		}
+//		for (int i = 0; i < n.ml.size(); i++) {
+//			n.ml.elementAt(i).accept(this);
+//		}
 		return null;
 	}
 
 	// Type t;
 	// Identifier i;
 	public Void visit(VarDecl n) {
-		n.t.accept(this);
-		n.i.accept(this);
+
+		if (this.currMethod == null) {
+			if (!this.currClass.containsVar(n.i.s)) {
+				this.currClass.addVar(n.i.s, n.t);
+			} else {
+				System.out.println("Variavel " + n.i.s + " ja existente na classe " + this.currClass.getId());
+				System.exit(0);
+			}
+		} else {
+			if (!this.currMethod.containsVar(n.i.s)) {
+				this.currMethod.addVar(n.i.s, n.t);
+			} else {
+				System.out.println("Variavel " + n.i.s + " ja existente no metodo " + this.currMethod.getId());
+				System.exit(0);
+			}
+		}
+
+
+
+
+//		n.t.accept(this);
+//		n.i.accept(this);
 		return null;
 	}
 
@@ -118,26 +156,56 @@ public class BuildSymbolTableVisitor implements IVisitor<Void> {
 	// StatementList sl;
 	// Exp e;
 	public Void visit(MethodDecl n) {
-		n.t.accept(this);
-		n.i.accept(this);
-		for (int i = 0; i < n.fl.size(); i++) {
-			n.fl.elementAt(i).accept(this);
+
+		if (!this.currClass.containsMethod(n.i.s)) {
+
+			this.currClass.addMethod(n.i.s, n.t);
+			this.currMethod = currClass.getMethod(n.i.s);
+
+			for (int i = 0; i < n.fl.size(); i++) {
+				n.fl.elementAt(i).accept(this);
+			}
+			for (int i = 0; i < n.vl.size(); i++) {
+				n.vl.elementAt(i).accept(this);
+			}
+			for (int i = 0; i < n.sl.size(); i++) {
+				n.sl.elementAt(i).accept(this);
+			}
+
+		} else {
+			System.out.println("Metodo " + n.i.s + " ja definido no escopo atual");
+			System.exit(0);
 		}
-		for (int i = 0; i < n.vl.size(); i++) {
-			n.vl.elementAt(i).accept(this);
-		}
-		for (int i = 0; i < n.sl.size(); i++) {
-			n.sl.elementAt(i).accept(this);
-		}
-		n.e.accept(this);
+
+
+//		n.t.accept(this);
+//		n.i.accept(this);
+//		for (int i = 0; i < n.fl.size(); i++) {
+//			n.fl.elementAt(i).accept(this);
+//		}
+//		for (int i = 0; i < n.vl.size(); i++) {
+//			n.vl.elementAt(i).accept(this);
+//		}
+//		for (int i = 0; i < n.sl.size(); i++) {
+//			n.sl.elementAt(i).accept(this);
+//		}
+//		n.e.accept(this);
 		return null;
 	}
 
 	// Type t;
 	// Identifier i;
 	public Void visit(Formal n) {
-		n.t.accept(this);
-		n.i.accept(this);
+
+		if (!this.currMethod.containsParam(n.i.s)) {
+			this.currMethod.addParam(n.i.s, n.t);
+		} else {
+			System.out.println("Parametro " + n.i.s + " ja existente no metodo " + this.currMethod.getId());
+			System.exit(0);
+		}
+
+//		n.t.accept(this);
+//		n.i.accept(this);
 		return null;
 	}
 
@@ -160,97 +228,97 @@ public class BuildSymbolTableVisitor implements IVisitor<Void> {
 
 	// StatementList sl;
 	public Void visit(Block n) {
-		for (int i = 0; i < n.sl.size(); i++) {
-			n.sl.elementAt(i).accept(this);
-		}
+//		for (int i = 0; i < n.sl.size(); i++) {
+//			n.sl.elementAt(i).accept(this);
+//		}
 		return null;
 	}
 
 	// Exp e;
 	// Statement s1,s2;
 	public Void visit(If n) {
-		n.e.accept(this);
-		n.s1.accept(this);
-		n.s2.accept(this);
+//		n.e.accept(this);
+//		n.s1.accept(this);
+//		n.s2.accept(this);
 		return null;
 	}
 
 	// Exp e;
 	// Statement s;
 	public Void visit(While n) {
-		n.e.accept(this);
-		n.s.accept(this);
+//		n.e.accept(this);
+//		n.s.accept(this);
 		return null;
 	}
 
 	// Exp e;
 	public Void visit(Print n) {
-		n.e.accept(this);
+//		n.e.accept(this);
 		return null;
 	}
 
 	// Identifier i;
 	// Exp e;
 	public Void visit(Assign n) {
-		n.i.accept(this);
-		n.e.accept(this);
+//		n.i.accept(this);
+//		n.e.accept(this);
 		return null;
 	}
 
 	// Identifier i;
 	// Exp e1,e2;
 	public Void visit(ArrayAssign n) {
-		n.i.accept(this);
-		n.e1.accept(this);
-		n.e2.accept(this);
+//		n.i.accept(this);
+//		n.e1.accept(this);
+//		n.e2.accept(this);
 		return null;
 	}
 
 	// Exp e1,e2;
 	public Void visit(And n) {
-		n.e1.accept(this);
-		n.e2.accept(this);
+//		n.e1.accept(this);
+//		n.e2.accept(this);
 		return null;
 	}
 
 	// Exp e1,e2;
 	public Void visit(LessThan n) {
-		n.e1.accept(this);
-		n.e2.accept(this);
+//		n.e1.accept(this);
+//		n.e2.accept(this);
 		return null;
 	}
 
 	// Exp e1,e2;
 	public Void visit(Plus n) {
-		n.e1.accept(this);
-		n.e2.accept(this);
+//		n.e1.accept(this);
+//		n.e2.accept(this);
 		return null;
 	}
 
 	// Exp e1,e2;
 	public Void visit(Minus n) {
-		n.e1.accept(this);
-		n.e2.accept(this);
+//		n.e1.accept(this);
+//		n.e2.accept(this);
 		return null;
 	}
 
 	// Exp e1,e2;
 	public Void visit(Times n) {
-		n.e1.accept(this);
-		n.e2.accept(this);
+//		n.e1.accept(this);
+//		n.e2.accept(this);
 		return null;
 	}
 
 	// Exp e1,e2;
 	public Void visit(ArrayLookup n) {
-		n.e1.accept(this);
-		n.e2.accept(this);
+//		n.e1.accept(this);
+//		n.e2.accept(this);
 		return null;
 	}
 
 	// Exp e;
 	public Void visit(ArrayLength n) {
-		n.e.accept(this);
+//		n.e.accept(this);
 		return null;
 	}
 
@@ -258,11 +326,11 @@ public class BuildSymbolTableVisitor implements IVisitor<Void> {
 	// Identifier i;
 	// ExpList el;
 	public Void visit(Call n) {
-		n.e.accept(this);
-		n.i.accept(this);
-		for (int i = 0; i < n.el.size(); i++) {
-			n.el.elementAt(i).accept(this);
-		}
+//		n.e.accept(this);
+//		n.i.accept(this);
+//		for (int i = 0; i < n.el.size(); i++) {
+//			n.el.elementAt(i).accept(this);
+//		}
 		return null;
 	}
 
